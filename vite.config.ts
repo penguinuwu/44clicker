d8c408676c0de3142ebde2ef2adc13d6a18a2c7f
Evan Cui <evan@good-meowning.com>
2024-11-11 02:28:26 -0500

    remove debugging in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
   plugins: [react()],
 })
